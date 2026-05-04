bf064d27b15a45ef548c8ca0962431151eb8531b
alione10-lm <aliouaneoussama89@gmail.com>
2026-05-04 12:49:21 +0100

    project structure

--- index.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = "b4af8081be5a413094f032ad0502d611";
-
-fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data.results, data.results.length));

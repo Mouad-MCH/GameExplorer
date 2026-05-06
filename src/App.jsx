1e62d015a0ca4738963315b8f373de2e10c98767
alione10-lm <aliouaneoussama89@gmail.com>
2026-05-06 13:00:01 +0100

    create creator details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/ui/Navbar";
+import CreatorDetails from "./pages/CreatorDetails";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/creators/:id" element={<CreatorDetails />} />
                 </Routes>
             </BrowserRouter>
         </div>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="flex  font-press items-center w-full border-b-secondary border-b p-5 fixed inset-x-0 top-0 bg-background z-20 justify-between ">
+        <nav className="flex  font-press items-center w-full border-b-secondary border-b p-5 fixed inset-x-0 top-0 bg-background z-50 justify-between ">
             <NavLink to="/">
                 <span className="text-white font-press font-bold text-sm tracking-widest uppercase">
                     GameExplorer

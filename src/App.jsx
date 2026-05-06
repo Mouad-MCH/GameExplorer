d7710ad93536f6334dc28211852dca2028582e9a
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-05-06 16:09:20 +0100

    creators part

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/ui/Navbar";
+import Creators from "./pages/Creators";//
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/creators" element={<Creators/>} />//
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 };
-
 export default App;
